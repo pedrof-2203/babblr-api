146e79029a312dc01642dce518cbb26d18be648b
pedro <pedroantoniofpa22@gmail.com>
2026-09-16 18:41:12 -0300

    feat: scaffold collab

---
+++ apps/collab/src/collab.module.ts
@@ -0,0 +1,10 @@
+import { Module } from '@nestjs/common';
+import { CollabController } from './collab.controller';
+import { CollabService } from './collab.service';
+
+@Module({
+  imports: [],
+  controllers: [CollabController],
+  providers: [CollabService]
+})
+export class CollabModule {}
